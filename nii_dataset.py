c5688e5a4f433e626429abfd9f70b424d313a251
Michael <CraneHere@163.com>
2023-08-23 14:46:45 +0800

    添加整体数据归一化操作

--- nii_dataset.py
+++ nii_dataset.py
@@ -7,6 +7,12 @@ import nibabel as nib
 DATA_CACHE = {}
 
 
+def norm(img):
+    mean = img.mean()
+    std = img.std()
+    return (img - mean) / std
+
+
 class NiiDataset(Dataset):
     classes = [
         "1 - NC",
@@ -21,14 +27,22 @@ class NiiDataset(Dataset):
             self.transform = None
 
     def __getitem__(self, index):
+        anomaly = False
         if self.img_path[index] in DATA_CACHE:
             img = DATA_CACHE[self.img_path[index]]
         else:
             img = nib.load(self.img_path[index])
             img = img.dataobj[:, :, :, 0]
+            img = norm(img)
+            # if img.shape[0] != 128:
+            #     anomaly = True
+            #     return None
+            # if img.shape[0] == 400:
+            #     anomaly = True
+            #     return None
             DATA_CACHE[self.img_path[index]] = img
 
-        idx = np.random.choice(range(img.shape[-1]), 50)
+        idx = np.random.choice(range(img.shape[-1]), 47)
         # idx.sort()
         img = img[:, :, idx]
         img = img.astype(np.float32)
@@ -37,11 +51,12 @@ class NiiDataset(Dataset):
         # img -= 1
 
         # print(img.shape)
-        if self.transform is not None and self.transform is not Ellipsis:
+        if self.transform is not None and self.transform is not Ellipsis and not anomaly:
             img = self.transform(image=img)['image']  # 用A进行transform之后返回一个字典，有两个属性，其中一个image属性就是图片
 
         img = img.transpose([2, 0, 1])
         # 如果这个nii文件是正常人的，label就标记为True，然后再将bool转化为int，也就是1
+        # print("dataset shape of img:", img.shape)
         return img, torch.from_numpy(np.array(int('NC' in self.img_path[index])))
 
     def __len__(self):
