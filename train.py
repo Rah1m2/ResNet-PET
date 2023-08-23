f992077275eba338075524f08fa8c7f244cfb90b
Michael <CraneHere@163.com>
2023-08-23 14:47:20 +0800

    将DEVICE变量改为大写

--- train.py
+++ train.py
@@ -3,22 +3,26 @@ import torch
 from torch.utils.tensorboard import SummaryWriter
 
 # 选择使用gpu还是cpu进行训练
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
 def run(opt):
     train(**vars(opt))
+
 
 def train(train_loader, model, criterion, optimizer, total_train_step):
     # 总loss
     total_loss = 0
-    # 训练步数
-    train_step = 0
     # 设置为训练模型
     model.train()
     for enum, (imgs, targets) in enumerate(train_loader):
+        if imgs == "empty":
+            print("skip...")
+            continue
         writer = SummaryWriter("logs")
         # 启用GPU
-        imgs = imgs.to(device)
-        targets = targets.to(device)
+        imgs = imgs.to(DEVICE)
+        targets = targets.to(DEVICE)
         # 放入模型进行训练
         outputs = model(imgs)
         loss = criterion(outputs, targets.long())
@@ -33,4 +37,4 @@ def train(train_loader, model, criterion, optimizer, total_train_step):
         if total_train_step % 10 == 0:
             print("训练次数：{}，Loss：{}, total_loss: {}".format(total_train_step, loss.item(), total_loss))
             writer.add_scalar("train_loss", loss.item(), total_train_step)
-    return total_loss/len(train_loader), total_train_step
+    return total_loss / len(train_loader), total_train_step
